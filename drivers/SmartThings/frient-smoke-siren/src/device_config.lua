aa4a4691194093c48bcf8376f0fa5e728aa29cea
Denis Zhmachenkov <dzhmachenkov@bmtp.life>
2023-02-13 15:42:41 +0200

    Added Range Extender [REXZB-111]

--- drivers/SmartThings/frient-smoke-siren/src/device_config.lua
+++ drivers/SmartThings/frient-smoke-siren/src/device_config.lua
@@ -1,7 +1,8 @@
 local FRIENT_DEVICE_FINGERPRINTS = {
   { mfr = "frient A/S", model = "SMSZB-120", subdriver = "smoke", ENDPOINT_SIREN = 0x23, ENDPOINT_TEMPERATURE = 0x26,
     ENDPOINT_TAMPER = 0x23 }, -- Siren, Temperature, Smoke
-  { mfr = "frient A/S", model = "SIRZB-110", subdriver = "siren", ENDPOINT_SIREN = 0x2B, ENDPOINT_TAMPER = 0x2B } -- Siren, Tamper
+  { mfr = "frient A/S", model = "SIRZB-110", subdriver = "siren", ENDPOINT_SIREN = 0x2B, ENDPOINT_TAMPER = 0x2B }, -- Siren, Tamper
+  { mfr = "frient A/S", model = "REXZB-111", subdriver = "siren", ENDPOINT_SIREN = 0x2B, ENDPOINT_TAMPER = 0x2B }, -- Siren, Tamper
 }
 
 return FRIENT_DEVICE_FINGERPRINTS
