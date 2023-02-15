f3b7eeb6a5b321494b0d169275424ecbb7be9993
Denis Zhmachenkov <dzhmachenkov@bmtp.life>
2023-02-15 10:52:23 +0200

    Changed Intelligent Smoke Alarm [SMSZB-120]

--- drivers/SmartThings/zigbee-smoke-detector/src/frient/init.lua
+++ drivers/SmartThings/zigbee-smoke-detector/src/frient/init.lua
@@ -11,14 +11,41 @@
 -- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 -- See the License for the specific language governing permissions and
 -- limitations under the License.
+local log                        = require "log"
+local st_utils                   = require "st.utils"
 
-local battery_defaults = require "st.zigbee.defaults.battery_defaults"
+--Capability
+local capabilities               = require "st.capabilities"
+local alarm                      = capabilities.alarm
 
-local FINGERPRINTS = {
+--Clusters
+local clusters                   = require "st.zigbee.zcl.clusters"
+local TemperatureMeasurement     = clusters.TemperatureMeasurement
+local IASWD                      = clusters.IASWD
+local SirenConfiguration         = IASWD.types.SirenConfiguration
+local WarningMode                = IASWD.types.WarningMode
+local Strobe                     = IASWD.types.Strobe
+local IaswdLevel                 = IASWD.types.IaswdLevel
+
+local Status                     = require "st.zigbee.generated.types.ZclStatus"
+local device_management          = require "st.zigbee.device_management"
+local zcl_global_commands        = require "st.zigbee.zcl.global_commands"
+local data_types                 = require "st.zigbee.data_types"
+local battery_defaults           = require "st.zigbee.defaults.battery_defaults"
+
+local battery_init               = battery_defaults.build_linear_voltage_init(2.3, 3.0)
+
+--Constants
+local ALARM_DEFAULT_MAX_DURATION = 0xF0
+local ALARM_LAST_DURATION        = "__last_duration"
+local ALARM_COMMAND              = "__alarm_cmd"
+local ALARM_STROBE_DUTY_CYCLE    = 40
+
+local FINGERPRINTS               = {
   { mfr = "frient A/S", model = "SMSZB-120" }
 }
 
-local can_handle = function(opts, driver, device)
+local function can_handle(opts, driver, device)
   for _, fingerprint in ipairs(FINGERPRINTS) do
     if device:get_manufacturer() == fingerprint.mfr and device:get_model() == fingerprint.model then
       return true
@@ -27,10 +54,138 @@ local can_handle = function(opts, driver, device)
   return false
 end
 
+local function emit_alarm_event(device, cmd)
+  if cmd == alarm.commands.off.NAME then
+    device:emit_event(capabilities.alarm.alarm.off())
+  else
+    if cmd == alarm.commands.siren.NAME then
+      device:emit_event(capabilities.alarm.alarm.siren())
+    elseif cmd == alarm.commands.strobe.NAME then
+      device:emit_event(capabilities.alarm.alarm.strobe())
+    else
+      device:emit_event(capabilities.alarm.alarm.both())
+    end
+  end
+end
+
+local function send_siren_command(device, warning_mode, warning_siren_level, strobe_active, strobe_level)
+  local max_duration     = device.preferences.warningDuration
+  local warning_duration = max_duration and max_duration or ALARM_DEFAULT_MAX_DURATION
+  local duty_cycle       = ALARM_STROBE_DUTY_CYCLE
+
+  device:set_field(ALARM_LAST_DURATION, warning_duration, { persist = true })
+
+  local siren_configuration = SirenConfiguration(0x00)
+
+  siren_configuration:set_warning_mode(warning_mode)
+  siren_configuration:set_strobe(strobe_active)
+  siren_configuration:set_siren_level(warning_siren_level)
+
+  device:send(
+      IASWD.server.commands.StartWarning(
+          device,
+          siren_configuration,
+          data_types.Uint16(warning_duration),
+          data_types.Uint8(duty_cycle),
+          data_types.Enum8(strobe_level)
+      )
+  )
+end
+
+local function temperature_measurement_measured_value_attr_config(device, reportable_change)
+  local attribute = {
+    cluster = TemperatureMeasurement.ID,
+    attribute = TemperatureMeasurement.attributes.MeasuredValue.ID,
+    minimum_interval = 30,
+    maximum_interval = 300,
+    data_type = TemperatureMeasurement.attributes.MeasuredValue.base_type,
+    reportable_change = device.preferences.temperatureSensitivity * 100
+  }
+  device:add_configured_attribute(attribute)
+  device:add_monitored_attribute(attribute)
+
+  for _, ep in pairs(device.zigbee_endpoints) do
+    if device:supports_server_cluster(attribute.cluster, ep.id) then
+      device:send(device_management.build_bind_request(device, attribute.cluster, device.driver.environment_info.hub_zigbee_eui, ep.id):to_endpoint(ep.id))
+      device:send(device_management.attr_config(device, attribute):to_endpoint(ep.id))
+    end
+  end
+end
+
+local function device_added(self, device, event, args)
+  device:emit_event(alarm.alarm.off())
+end
+
+local function device_init(self, device, event, args)
+  battery_init(self, device, event, args)
+end
+
+local function device_do_configure(self, device, event, args)
+  device:send(IASWD.attributes.MaxDuration:write(device, device.preferences.warningDuration or ALARM_DEFAULT_MAX_DURATION))
+
+  temperature_measurement_measured_value_attr_config(device, device.preferences.temperatureSensitivity)
+
+  device:configure()
+  device:refresh()
+end
+
+local function device_info_changed(self, device, event, args)
+  if args.old_st_store.preferences.warningDuration ~= device.preferences.warningDuration then
+    device:send(IASWD.attributes.MaxDuration:write(device, device.preferences.warningDuration or ALARM_DEFAULT_MAX_DURATION))
+  end
+  if args.old_st_store.preferences.temperatureSensitivity ~= device.preferences.temperatureSensitivity then
+    temperature_measurement_measured_value_attr_config(device, device.preferences.temperatureSensitivity)
+  end
+end
+
+local function default_response_handler(driver, device, zigbee_message)
+  local is_success = zigbee_message.body.zcl_body.status.value
+  local command    = zigbee_message.body.zcl_body.cmd.value
+  local alarm_ev   = device:get_field(ALARM_COMMAND)
+
+  if command == IASWD.server.commands.StartWarning.ID and is_success == Status.SUCCESS then
+    if alarm_ev ~= alarm.commands.off.NAME then
+      emit_alarm_event(device, alarm_ev)
+      local lastDuration = device:get_field(ALARM_LAST_DURATION) or ALARM_DEFAULT_MAX_DURATION
+      device.thread:call_with_delay(lastDuration, function(d)
+        device:emit_event(capabilities.alarm.alarm.off())
+      end)
+    else
+      emit_alarm_event(device, alarm.commands.off.NAME)
+    end
+  end
+end
+
+local function siren_switch_off_handler(driver, device, command)
+  device:set_field(ALARM_COMMAND, command.command, { persist = true })
+  send_siren_command(device, WarningMode.STOP, IaswdLevel.LOW_LEVEL, Strobe.NO_STROBE, IaswdLevel.LOW_LEVEL)
+end
+
+local function siren_alarm_siren_handler(driver, device, command)
+  device:set_field(ALARM_COMMAND, command.command, { persist = true })
+  send_siren_command(device, WarningMode.BURGLAR, IaswdLevel.VERY_HIGH_LEVEL, Strobe.NO_STROBE, IaswdLevel.LOW_LEVEL)
+end
+
 local frient_smoke_detector = {
   NAME = "Freint Smoke Detector",
   lifecycle_handlers = {
-    init = battery_defaults.build_linear_voltage_init(2.3, 3.0)
+    added = device_added,
+    init = device_init,
+    doConfigure = device_do_configure,
+    infoChanged = device_info_changed
+  },
+  zigbee_handlers = {
+    global = {
+      [IASWD.ID] = {
+        [zcl_global_commands.DEFAULT_RESPONSE_ID] = default_response_handler
+      }
+    }
+  },
+  capability_handlers = {
+    [alarm.ID] = {
+      [alarm.commands.off.NAME] = siren_switch_off_handler,
+      [alarm.commands.siren.NAME] = siren_alarm_siren_handler
+    }
   },
   can_handle = can_handle
 }
--- drivers/SmartThings/zigbee-smoke-detector/src/test/test_frient_smoke_detector.lua
+++ drivers/SmartThings/zigbee-smoke-detector/src/test/test_frient_smoke_detector.lua
@@ -13,29 +13,42 @@
 -- limitations under the License.
 
 -- Mock out globals
-local test = require "integration_test"
-local clusters = require "st.zigbee.zcl.clusters"
-local IASZone = clusters.IASZone
-local PowerConfiguration = clusters.PowerConfiguration
-local capabilities = require "st.capabilities"
-local zigbee_test_utils = require "integration_test.zigbee_test_utils"
-local IasEnrollResponseCode = require "st.zigbee.generated.zcl_clusters.IASZone.types.EnrollResponseCode"
-local base64 = require "st.base64"
-local t_utils = require "integration_test.utils"
+local test                   = require "integration_test"
+local cluster_base           = require "st.zigbee.cluster_base"
+local data_types             = require "st.zigbee.data_types"
+local clusters               = require "st.zigbee.zcl.clusters"
+local Basic                  = clusters.Basic
+local IASZone                = clusters.IASZone
+local IASWD                  = clusters.IASWD
 
-local ZoneStatusAttribute = IASZone.attributes.ZoneStatus
+local IasZoneStatus          = require "st.zigbee.generated.types.IasZoneStatus"
+local SirenConfiguration     = IASWD.types.SirenConfiguration
+local PowerConfiguration     = clusters.PowerConfiguration
+local TemperatureMeasurement = clusters.TemperatureMeasurement
+local capabilities           = require "st.capabilities"
+local zigbee_test_utils      = require "integration_test.zigbee_test_utils"
+local IasEnrollResponseCode  = require "st.zigbee.generated.zcl_clusters.IASZone.types.EnrollResponseCode"
+local t_utils                = require "integration_test.utils"
 
-local mock_device = test.mock_device.build_test_zigbee_device(
-  { profile = t_utils.get_profile_definition("smoke-battery.yml"),
-    zigbee_endpoints = {
-      [1] = {
-        id = 1,
-        manufacturer = "frient A/S",
-        model = "SMSZB-120",
-        server_clusters = {0x0000,0x0001,0x0003,0x000F,0x0020,0x0500,0x0502}
+local mock_device            = test.mock_device.build_test_zigbee_device(
+    { profile = t_utils.get_profile_definition("smoke-battery-temperature-siren.yml"),
+      zigbee_endpoints = {
+        [0x01] = {
+          id = 0x01,
+          manufacturer = "frient A/S",
+          model = "SMSZB-120",
+          server_clusters = { 0x0003, 0x0005, 0x0006 }
+        },
+        [0x23] = {
+          id = 1,
+          server_clusters = { 0x0000, 0x0001, 0x0003, 0x000F, 0x0020, 0x0500, 0x0502 }
+        },
+        [0x26] = {
+          id = 1,
+          server_clusters = { 0x0000, 0x0003, 0x0402 }
+        }
       }
     }
-  }
 )
 
 zigbee_test_utils.prepare_zigbee_env_info()
@@ -51,12 +64,12 @@ test.register_message_test(
       {
         channel = "zigbee",
         direction = "receive",
-        message = { mock_device.id, PowerConfiguration.attributes.BatteryVoltage:build_test_attr_report(mock_device, 24) }
+        message = { mock_device.id, PowerConfiguration.attributes.BatteryVoltage:build_test_attr_report(mock_device, 26) }
       },
       {
         channel = "capability",
         direction = "send",
-        message = mock_device:generate_test_message("main", capabilities.battery.battery(14))
+        message = mock_device:generate_test_message("main", capabilities.battery.battery(43))
       }
     }
 )
@@ -64,15 +77,23 @@ test.register_message_test(
 test.register_coroutine_test(
     "Health check should check all relevant attributes",
     function()
-      test.socket.device_lifecycle:__queue_receive({mock_device.id, "added"})
-      test.wait_for_events()
+      test.socket.device_lifecycle:__queue_receive({ mock_device.id, "added" })
 
       test.mock_time.advance_time(50000) -- battery is 21600 for max reporting interval
       test.socket.zigbee:__set_channel_ordering("relaxed")
+      test.socket.capability:__expect_send(
+          mock_device:generate_test_message("main", capabilities.alarm.alarm.off())
+      )
       test.socket.zigbee:__expect_send(
           {
             mock_device.id,
             PowerConfiguration.attributes.BatteryVoltage:read(mock_device)
+          }
+      )
+      test.socket.zigbee:__expect_send(
+          {
+            mock_device.id,
+            TemperatureMeasurement.attributes.MeasuredValue:read(mock_device)
           }
       )
       test.socket.zigbee:__expect_send(
@@ -92,67 +113,32 @@ test.register_coroutine_test(
 
 test.register_coroutine_test(
     "Configure should configure all necessary attributes",
-    function ()
+    function()
       test.socket.zigbee:__set_channel_ordering("relaxed")
-      test.socket.device_lifecycle:__queue_receive({ mock_device.id, "added"})
-      test.socket.device_lifecycle:__queue_receive({ mock_device.id, "doConfigure"})
+      test.socket.device_lifecycle:__queue_receive({ mock_device.id, "added" })
+      test.socket.device_lifecycle:__queue_receive({ mock_device.id, "doConfigure" })
+      test.socket.capability:__expect_send(
+          mock_device:generate_test_message("main", capabilities.alarm.alarm.off())
+      )
       test.socket.zigbee:__expect_send({
-                                         mock_device.id,
-                                         PowerConfiguration.attributes.BatteryVoltage:read(mock_device)
-                                       })
+        mock_device.id,
+        IASWD.attributes.MaxDuration:write(mock_device, 0xF0)
+      })
       test.socket.zigbee:__expect_send({
-                                         mock_device.id,
-                                         IASZone.attributes.ZoneStatus:read(mock_device)
-                                       })
+        mock_device.id,
+        IASZone.attributes.IASCIEAddress:write(
+            mock_device,
+            zigbee_test_utils.mock_hub_eui
+        )
+      })
       test.socket.zigbee:__expect_send({
-                                         mock_device.id,
-                                         PowerConfiguration.attributes.BatteryVoltage:configure_reporting(
-                                             mock_device,
-                                             30,
-                                             21600,
-                                             1
-                                         )
-                                       })
-      test.socket.zigbee:__expect_send({
-                                         mock_device.id,
-                                         zigbee_test_utils.build_bind_request(
-                                             mock_device,
-                                             zigbee_test_utils.mock_hub_eui,
-                                             PowerConfiguration.ID
-                                         )
-                                       })
-      test.socket.zigbee:__expect_send({
-                                         mock_device.id,
-                                         IASZone.attributes.IASCIEAddress:write(
-                                             mock_device,
-                                             zigbee_test_utils.mock_hub_eui
-                                         )
-                                       })
-      test.socket.zigbee:__expect_send({
-                                         mock_device.id,
-                                         IASZone.server.commands.ZoneEnrollResponse(
-                                             mock_device,
-                                             IasEnrollResponseCode.SUCCESS,
-                                             0x00
-                                         )
-                                       })
-      test.socket.zigbee:__expect_send({
-                                         mock_device.id,
-                                         IASZone.attributes.ZoneStatus:configure_reporting(
-                                             mock_device,
-                                             0,
-                                             180,
-                                             0
-                                         )
-                                       })
-      test.socket.zigbee:__expect_send({
-                                         mock_device.id,
-                                         zigbee_test_utils.build_bind_request(
-                                             mock_device,
-                                             zigbee_test_utils.mock_hub_eui,
-                                             IASZone.ID
-                                         )
-                                       })
+        mock_device.id,
+        IASZone.server.commands.ZoneEnrollResponse(
+            mock_device,
+            IasEnrollResponseCode.SUCCESS,
+            0x00
+        )
+      })
 
       mock_device:expect_metadata_update({ provisioning_state = "PROVISIONED" })
     end
@@ -164,7 +150,12 @@ test.register_message_test(
       {
         channel = "device_lifecycle",
         direction = "receive",
-        message = {mock_device.id, "added"}
+        message = { mock_device.id, "added" }
+      },
+      {
+        channel = "capability",
+        direction = "send",
+        message = mock_device:generate_test_message("main", capabilities.alarm.alarm.off())
       },
       {
         channel = "capability",
@@ -173,7 +164,7 @@ test.register_message_test(
           mock_device.id,
           { capability = "refresh", component = "main", command = "refresh", args = {} }
         }
-      },
+      }--[[,
       {
         channel = "zigbee",
         direction = "send",
@@ -189,11 +180,133 @@ test.register_message_test(
           mock_device.id,
           IASZone.attributes.ZoneStatus:read(mock_device)
         }
-      },
+      }]]
     },
     {
       inner_block_ordering = "relaxed"
     }
+)
+
+test.register_message_test(
+    "Capability(alarm) command(off) on should be handled",
+    {
+      {
+        channel = "capability",
+        direction = "receive",
+        message = { mock_device.id, { capability = "alarm", component = "main", command = "off", args = { } } }
+      },
+      {
+        channel = "zigbee",
+        direction = "send",
+        message = { mock_device.id, IASWD.server.commands.StartWarning(mock_device,
+            SirenConfiguration(00),
+            data_types.Uint16(0x00F0),
+            data_types.Uint8(0x28),
+            data_types.Enum8(00)) }
+      }
+    }
+)
+
+test.register_message_test(
+    "Capability(alarm) command(siren) on should be handled",
+    {
+      {
+        channel = "capability",
+        direction = "receive",
+        message = { mock_device.id, { capability = "alarm", component = "main", command = "siren", args = { } } }
+      },
+      {
+        channel = "zigbee",
+        direction = "send",
+        message = { mock_device.id, IASWD.server.commands.StartWarning(mock_device,
+            SirenConfiguration(0x13),
+            data_types.Uint16(0x00F0),
+            data_types.Uint8(0x28),
+            data_types.Enum8(00)) }
+      }
+    }
+)
+
+test.register_message_test(
+    "IASZone status(alarm1) report should be handled",
+    {
+      {
+        channel = "zigbee",
+        direction = "receive",
+        message = { mock_device.id, IASZone.attributes.ZoneStatus:build_test_attr_report(mock_device, IasZoneStatus(0x0001)) }
+      },
+      {
+        channel = "capability",
+        direction = "send",
+        message = mock_device:generate_test_message("main", capabilities.smokeDetector.smoke.detected())
+      }
+    }
+)
+
+test.register_message_test(
+    "IASZone status(none) report should be handled",
+    {
+      {
+        channel = "zigbee",
+        direction = "receive",
+        message = { mock_device.id, IASZone.attributes.ZoneStatus:build_test_attr_report(mock_device, IasZoneStatus(0x0000)) }
+      },
+      {
+        channel = "capability",
+        direction = "send",
+        message = mock_device:generate_test_message("main", capabilities.smokeDetector.smoke.clear())
+      }
+    }
+)
+
+test.register_message_test(
+    "ZoneStatusChangeNotification(alarm1) should be handled",
+    {
+      {
+        channel = "zigbee",
+        direction = "receive",
+        message = { mock_device.id, IASZone.commands.ZoneStatusChangeNotification.build_test_rx(mock_device,
+            IasZoneStatus(0x0001),
+            0,
+            0) }
+      },
+      {
+        channel = "capability",
+        direction = "send",
+        message = mock_device:generate_test_message("main", capabilities.smokeDetector.smoke.detected())
+      }
+    }
+)
+
+test.register_message_test(
+    "ZoneStatusChangeNotification(none) should be handled",
+    {
+      {
+        channel = "zigbee",
+        direction = "receive",
+        message = { mock_device.id, IASZone.commands.ZoneStatusChangeNotification.build_test_rx(mock_device,
+            IasZoneStatus(0x0000),
+            0,
+            0) }
+      },
+      {
+        channel = "capability",
+        direction = "send",
+        message = mock_device:generate_test_message("main", capabilities.smokeDetector.smoke.clear())
+      }
+    }
+)
+
+test.register_coroutine_test(
+    "Preference a max duration should be handled",
+    function()
+      test.socket.device_lifecycle:__queue_receive(mock_device:generate_info_changed({ preferences = { temperatureSensitivity = 0.1, warningDuration = 50 } }))
+
+      test.socket.zigbee:__expect_send({
+        mock_device.id,
+        IASWD.attributes.MaxDuration:write(mock_device, 0x32)
+      })
+    end
 )
 
 test.run_registered_tests()
