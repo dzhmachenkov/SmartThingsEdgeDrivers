30cf722b37231ca9e6a71c8fecf96d1388df1ad8
Denis Zhmachenkov <dzhmachenkov@bmtp.life>
2023-02-19 16:03:46 +0200

    Move BasicInput to subdriver

--- drivers/SmartThings/frient-button/src/frient-smart/init.lua
+++ drivers/SmartThings/frient-button/src/frient-smart/init.lua
@@ -1,14 +1,32 @@
-local zcl_clusters               = require "st.zigbee.zcl.clusters"
+local capabilities               = require "st.capabilities"
+
 local cluster_base               = require "st.zigbee.cluster_base"
 local data_types                 = require "st.zigbee.data_types"
+local device_management          = require "st.zigbee.device_management"
 
+local zcl_clusters               = require "st.zigbee.zcl.clusters"
+local BasicInput                 = zcl_clusters.BasicInput
 local OnOff                      = zcl_clusters.OnOff
+
+local log                        = require "log"
+local st_utils                   = require "st.utils"
 
 local FRIENT_DEVICE_FINGERPRINTS = require "device_config"
 
 local MFG_CODE                   = 0x1015
 local BUTTON_LED_COLOR           = 0x8002
 local BUTTON_PRESS_DELAY         = 0x8001
+
+local CONFIGURATION              = {
+  {
+    cluster = BasicInput.ID,
+    attribute = BasicInput.attributes.PresentValue.ID,
+    minimum_interval = 0,
+    maximum_interval = 600,
+    data_type = BasicInput.attributes.PresentValue.base_type,
+    reportable_change = 1
+  }
+}
 
 --- @param opts table A table containing optional arguments that can be used to determine if something is handleable
 --- @param driver ZigbeeDriver The current driver running containing necessary context for execution
@@ -32,6 +50,30 @@ end
 
 --- @param driver ZigbeeDriver The current driver running containing necessary context for execution
 --- @param device st.zigbee.Device The device this message was received from containing identifying information
+--- @param value st.zigbee.data_types.Uint16 the value of the event
+--- @param zb_rx st.zigbee.ZigbeeMessageRx the full message this report came in
+local function present_value_attr_handler(driver, device, value, zb_rx)
+  local event
+  local additional_fields = {
+    state_change = true
+  }
+  if value.value == true then
+    event = capabilities.button.button.pushed(additional_fields)
+    device:emit_event_for_endpoint(zb_rx.address_header.src_endpoint.value, event)
+  end
+end
+
+local function device_init(self, device, event, args)
+  self.lifecycle_handlers.init(self, device, event, args)
+
+  for _, attribute in ipairs(CONFIGURATION) do
+    device:add_configured_attribute(attribute)
+    device:add_monitored_attribute(attribute)
+  end
+end
+
+--- @param driver ZigbeeDriver The current driver running containing necessary context for execution
+--- @param device st.zigbee.Device The device this message was received from containing identifying information
 --- @param event string The lifecycle event name
 --- @param args table Table containing information relevant to the lifecycle event
 local function device_info_changed(driver, device, event, args)
@@ -50,12 +92,16 @@ end
 local frient_smart_button = {
   NAME = "Frient Smart Button",
   lifecycle_handlers = {
+    init = device_init,
     infoChanged = device_info_changed,
   },
   zigbee_handlers = {
     cluster = {
     },
     attr = {
+      [BasicInput.ID] = {
+        [BasicInput.attributes.PresentValue.ID] = present_value_attr_handler
+      }
     }
   },
   capability_handlers = {
--- drivers/SmartThings/frient-button/src/init.lua
+++ drivers/SmartThings/frient-button/src/init.lua
@@ -1,15 +1,15 @@
-local capabilities      = require "st.capabilities"
-local ZigbeeDriver      = require "st.zigbee"
-local zcl_clusters      = require "st.zigbee.zcl.clusters"
-local defaults          = require "st.zigbee.defaults"
-local battery_defaults  = require "st.zigbee.defaults.battery_defaults"
-local device_management = require "st.zigbee.device_management"
-local constants         = require "st.zigbee.constants"
+local capabilities     = require "st.capabilities"
+local ZigbeeDriver     = require "st.zigbee"
+local zcl_clusters     = require "st.zigbee.zcl.clusters"
+local defaults         = require "st.zigbee.defaults"
+local battery_defaults = require "st.zigbee.defaults.battery_defaults"
+local constants        = require "st.zigbee.constants"
 
-local IASZone           = zcl_clusters.IASZone
-local BasicInput        = zcl_clusters.BasicInput
+local IASZone          = zcl_clusters.IASZone
 
-local battery_table     = {
+local log              = require "log"
+
+local battery_table    = {
   [2.90] = 100,
   [2.80] = 80,
   [2.75] = 60,
@@ -24,21 +24,6 @@ local battery_table     = {
   [1.90] = 0,
   [0.00] = 0
 }
-
---- @param driver ZigbeeDriver The current driver running containing necessary context for execution
---- @param device st.zigbee.Device The device this message was received from containing identifying information
---- @param value st.zigbee.data_types.Uint16 the value of the event
---- @param zb_rx st.zigbee.ZigbeeMessageRx the full message this report came in
-local function present_value_attr_handler(driver, device, value, zb_rx)
-  local event
-  local additional_fields = {
-    state_change = true
-  }
-  if value.value == true then
-    event = capabilities.button.button.pushed(additional_fields)
-    device:emit_event_for_endpoint(zb_rx.address_header.src_endpoint.value, event)
-  end
-end
 
 --- @param driver ZigbeeDriver The current driver running containing necessary context for execution
 --- @param zone_status st.zigbee.zcl.types.IasZoneStatus 2 byte bitmap zoneStatus attribute value of the IAS Zone cluster
@@ -83,11 +68,6 @@ local function device_init(driver, device)
   battery_defaults.enable_battery_voltage_table(device, battery_table)
 end
 
-local function device_configure(self, device)
-  device:send(device_management.build_bind_request(device, BasicInput.ID, self.environment_info.hub_zigbee_eui))
-  device:send(BasicInput.attributes.PresentValue:configure_reporting(device, 0, 600, 1))
-end
-
 local frient_button_driver = {
   NAME = "frient button driver",
   supported_capabilities = {
@@ -99,8 +79,7 @@ local frient_button_driver = {
   },
   lifecycle_handlers = {
     added = device_added,
-    init = device_init,
-    doConfigure = device_configure
+    init = device_init
   },
   zigbee_handlers = {
     cluster = {
@@ -109,16 +88,12 @@ local frient_button_driver = {
       }
     },
     attr = {
-      [BasicInput.ID] = {
-        [BasicInput.attributes.PresentValue.ID] = present_value_attr_handler
-      },
       [IASZone.ID] = {
         [IASZone.attributes.ZoneStatus.ID] = ias_zone_status_attr_handler
       }
     },
   },
   capability_handlers = {
-
   },
   ias_zone_configuration_method = constants.IAS_ZONE_CONFIGURE_TYPE.AUTO_ENROLL_RESPONSE,
 }
